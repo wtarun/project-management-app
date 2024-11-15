89696a0d93da332fb80dbbcf4018cc93fb86d378
Wajjala Tarun <wajjala@Wajjalas-MacBook-Air.local>
2024-11-15 16:57:51 +0530

    Added the sidebar component skeleton to the project

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+export default function ProjectsSidebar() {
+    return (
+      <aside>
+        <h2>Projects Sidebar</h2>
+        <div>
+            <button>+ Add Projects</button>
+        </div>
+        <ul></ul>
+      </aside>
+    );
+}

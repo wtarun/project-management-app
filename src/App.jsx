5442e2a17e90206e7d3ea8cab5ccb1e77b0244d1
Wajjala Tarun <wajjala@Wajjalas-MacBook-Air.local>
2024-11-16 11:48:54 +0530

    Added styling to sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import ProjectsSidebar from "./components/Sidebar";
+import ProjectsSidebar from './components/ProjectsSidebar.jsx';
 
 function App() {
   return (
-    <main>
+    <main className="h-screen my-8">
       <ProjectsSidebar />
     </main>
   );

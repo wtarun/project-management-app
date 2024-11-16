b9707fbb9783731ec85e6c25d17f5b9724b6c3f0
Wajjala Tarun <wajjala@Wajjalas-MacBook-Air.local>
2024-11-16 12:38:22 +0530

    Added conditional rendering for NoContent and NewProject component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import ProjectsSidebar from './components/ProjectsSidebar.jsx';
+import NewProject from './components/NewProject.jsx';
+import ProjectsSidebar from "./components/Sidebar.jsx"
+import NoProjectSelected from './components/NoProjectSelected.jsx';
+import { useState } from 'react';
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  const handleAddNewProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    })
+  }
+
+  let content;
+
+  if(projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleAddNewProject}/>
+  } else if(projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  }
+
   return (
-    <main className="h-screen my-8">
-      <ProjectsSidebar />
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onStartAddProject={handleAddNewProject}/>
+      {content}
     </main>
   );
 }

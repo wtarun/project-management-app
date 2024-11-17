eaebe1557e78a113d1b533c5c7531ef71460f625
Wajjala Tarun <wajjala@Wajjalas-MacBook-Air.local>
2024-11-17 14:33:08 +0530

    Added a modl which pops up when an invalid or not input is provided

--- src/App.jsx
+++ src/App.jsx
@@ -43,9 +43,9 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar 
-      onStartAddProject={handleAddNewProject} 
-      projects = {projectsState.projects}
+      <ProjectsSidebar
+        onStartAddProject={handleAddNewProject}
+        projects={projectsState.projects}
       />
       {content}
     </main>
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,10 +1,12 @@
-import Input from './Input.jsx';
-import { useRef } from 'react';
+import Input from "./Input.jsx";
+import { useRef } from "react";
+import Modal from "./Modal.jsx";
 
-export default function NewProject({getSavedData}) {
+export default function NewProject({ getSavedData }) {
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
+  const modal = useRef();
 
   const handleSave = () => {
     const enteredTitle = title.current.value;
@@ -12,33 +14,55 @@ export default function NewProject({getSavedData}) {
     const enteredDueDate = dueDate.current.value;
 
     //validations..
+    if (
+      enteredTitle.trim() === '' ||
+      enteredDescription.trim() === '' ||
+      enteredDueDate.trim() === ''
+    ) {
+      modal.current.open();
+      return;
+    }
 
     getSavedData({
       title: enteredTitle,
       description: enteredDescription,
-      dueDate: enteredDueDate
+      dueDate: enteredDueDate,
     }); //props
-
-  }
+  };
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">Cancel</button>
-        </li>
-        <li>
-          <button 
-          className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-          onClick={handleSave}
-          >Save</button>
-        </li>
-      </menu>
-      <div>
-        <Input ref={title} label="Title" />
-        <Input ref={description}  label="Description" textarea />
-        <Input ref={dueDate} type="date" label="Due Date" />
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          Oops ... looks like you forgot to enter a value.
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input field.
+        </p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input ref={dueDate} type="date" label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
